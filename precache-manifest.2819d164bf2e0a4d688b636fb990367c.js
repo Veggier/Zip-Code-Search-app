a3c2f0ab9b67b58972cc6acf8f24f735227fe22d
zhongcai jiang <jiangzhongcai123@gmail.com>
2020-06-05 13:46:56 -0400

    Updates

---
+++ precache-manifest.2819d164bf2e0a4d688b636fb990367c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3a6856f6d040699474c936a2739af3",
+    "url": "/Zip-Code-Search-app/index.html"
+  },
+  {
+    "revision": "7be93000adfaaea7fd20",
+    "url": "/Zip-Code-Search-app/static/css/main.2f282f52.chunk.css"
+  },
+  {
+    "revision": "b08924e97ef4323e7b96",
+    "url": "/Zip-Code-Search-app/static/js/2.94dcb556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Zip-Code-Search-app/static/js/2.94dcb556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be93000adfaaea7fd20",
+    "url": "/Zip-Code-Search-app/static/js/main.15daf1ba.chunk.js"
+  },
+  {
+    "revision": "8a9ca1d6d07f76007d85",
+    "url": "/Zip-Code-Search-app/static/js/runtime-main.ea46784e.js"
+  },
+  {
+    "revision": "f1fcd246fd9ee074b4a93de3615a3750",
+    "url": "/Zip-Code-Search-app/static/media/Background-Images-2.f1fcd246.jpg"
+  }
+]);
